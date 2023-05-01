a8ef3f50571ef02e04c0c247d4428c11e775b248
unknown <abdulmajid1m2@gmail.com>
2023-05-01 18:14:11 +0500

    new wbs mobile screen start

--- Backend/cacheManager.js
+++ Backend/cacheManager.js
@@ -1,16 +1,16 @@
-import Redis from "ioredis";
+// import Redis from "ioredis";
 
-const client = new Redis({
-    host: "172.21.73.77",
-    port: 6380,
-});
+// const client = new Redis({
+//     host: "172.21.73.77",
+//     port: 6380,
+// });
 
-client.on("connect", () => {
-    console.log("Connected to Redis server");
-});
+// client.on("connect", () => {
+//     console.log("Connected to Redis server");
+// });
 
-client.on("error", (err) => {
-    console.log("Error connecting to Redis server:", err);
-});
+// client.on("error", (err) => {
+//     console.log("Error connecting to Redis server:", err);
+// });
 
-export { client };
+// export { client };

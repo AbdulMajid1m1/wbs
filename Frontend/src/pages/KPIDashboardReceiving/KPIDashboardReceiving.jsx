173972dd494761336d0cfa3e9a893d37ef3fbb61
unknown <abdulmajid1m2@gmail.com>
2023-05-03 23:52:47 +0500

    inset api updated stats api added v3

--- Frontend/src/pages/KPIDashboardReceiving/KPIDashboardReceiving.jsx
+++ Frontend/src/pages/KPIDashboardReceiving/KPIDashboardReceiving.jsx
@@ -82,7 +82,7 @@ const KPIDashboardRece = () => {
             </div>
             <div className="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
               <div className="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
-                {/* <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg> */}
+                {/* <svg width="30" height="30" fill="no/ne" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg> */}
                 <img src={shipment} className='' alt='' />
               </div>
               <div className="text-right">

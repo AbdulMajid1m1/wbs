eb2521b1658ced61e93c75758fd22c8028d21bfc
HasnainAhmad12 <hasnainbangash03@gmail.com>
2023-05-04 11:32:18 +0500

    Commit

--- Frontend/src/components/Charts/TotalShipment.jsx
+++ Frontend/src/components/Charts/TotalShipment.jsx
@@ -1,66 +1,155 @@
-import React from 'react'
-// import { data } from "../Config/Data";
+// import React from 'react'
+// // import { data } from "../Config/Data";
 
-// Preparing the chart data
-export const data = [
-    {
-      label: "Venezuela",
-      value: "290"
-    },
-    {
-      label: "Saudi",
-      value: "260"
-    },
-    {
-      label: "Canada",
-      value: "180"
-    },
-    {
-      label: "Iran",
-      value: "140"
-    },
-    {
-      label: "Russia",
-      value: "115"
-    },
-    {
-      label: "UAE",
-      value: "100"
-    },
-    {
-      label: "US",
-      value: "30"
-    },
-    {
-      label: "China",
-      value: "30"
-    }
-  ];
+// // Preparing the chart data
+// export const data = [
+//     {
+//       label: "Venezuela",
+//       value: "290"
+//     },
+//     {
+//       label: "Saudi",
+//       value: "260"
+//     },
+//     {
+//       label: "Canada",
+//       value: "180"
+//     },
+//     {
+//       label: "Iran",
+//       value: "140"
+//     },
+//     {
+//       label: "Russia",
+//       value: "115"
+//     },
+//     {
+//       label: "UAE",
+//       value: "100"
+//     },
+//     {
+//       label: "US",
+//       value: "30"
+//     },
+//     {
+//       label: "China",
+//       value: "30"
+//     }
+//   ];
 
 
   
-const TotalShipment = ({ReactFC}) => {
-    const chartConfigs = {
-        type: "column2d", // The chart type
-        width: "100%", // Width of the chart
-        height: "400", // Height of the chart
-        dataFormat: "json", // Data type
-        dataSource: {
-          // Chart Configuration
-          chart: {
-            caption: "Countries With Most Oil Reserves [2017-18]",    //Set the chart caption
-            subCaption: "In MMbbl = One Million barrels",             //Set the chart subcaption
-            xAxisName: "Country",           //Set the x-axis name
-            yAxisName: "Reserves (MMbbl)",  //Set the y-axis name
-            numberSuffix: "K",
-            theme: "fusion"                 //Set the theme for your chart
-          },
-          // Chart Data - from step 2
-          data: data
-        }
-      };
+// const TotalShipment = ({ReactFC}) => {
+//     const chartConfigs = {
+//         type: "column2d", // The chart type
+//         width: "100%", // Width of the chart
+//         height: "400", // Height of the chart
+//         dataFormat: "json", // Data type
+//         dataSource: {
+//           // Chart Configuration
+//           chart: {
+//             caption: "Countries With Most Oil Reserves [2017-18]",    //Set the chart caption
+//             subCaption: "In MMbbl = One Million barrels",             //Set the chart subcaption
+//             xAxisName: "Country",           //Set the x-axis name
+//             yAxisName: "Reserves (MMbbl)",  //Set the y-axis name
+//             numberSuffix: "K",
+//             theme: "fusion"                 //Set the theme for your chart
+//           },
+//           // Chart Data - from step 2
+//           data: data
+//         }
+//       };
     
-  return (<ReactFC {...chartConfigs}/>)
+//   return (<ReactFC {...chartConfigs}/>)
+// }
+
+// export default TotalShipment
+
+
+// import React, { useState, useEffect } from 'react'
+// import axios from 'axios'
+
+// export const TotalShipment = ({ ReactFC }) => {
+//   const [data, setData] = useState([])
+
+//   useEffect(() => {
+//     axios.get('https://dummyjson.com/products')
+//       .then(response => {
+//         const chartData = [{
+//           label: response.data.brand,
+//           value: response.data.stock
+//         }]
+//         setData(chartData)
+//       })
+//       .catch(error => console.log(error))
+//   }, [])
+
+//   const chartConfigs = {
+//     type: 'column2d',
+//     width: '100%',
+//     height: '400',
+//     dataFormat: 'json',
+//     dataSource: {
+//       chart: {
+//         caption: 'Stock by Brand',
+//         subCaption: 'Number of units in stock by brand',
+//         xAxisName: 'Brand',
+//         // yAxisName: 'Stock (units)',
+//         theme: 'fusion'
+//       },
+//       data: data
+//     }
+//   }
+
+//   return (
+//     <ReactFC {...chartConfigs} />
+//   )
+// }
+
+// export default TotalShipment
+
+
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const MyChart = ({ReactFC}) => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    axios.get('https://dummyjson.com/products')
+      .then(res => {
+        const productsData = res.data.products.map(product => {
+          return {
+            label: product.title,
+            value: product.price
+          }
+        })
+        setProducts(productsData)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  const chartConfigs = {
+    type: "column2d",
+    width: "100%",
+    height: "400",
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption: "Product Prices",
+        subCaption: "Prices of various products",
+        xAxisName: "Product",
+        yAxisName: "Price (USD)",
+        numberSuffix: "$",
+        theme: "fusion"
+      },
+      data: products
+    }
+  }
+
+  return (
+    <ReactFC {...chartConfigs} />
+  )
 }
 
-export default TotalShipment
+export default MyChart
--- Frontend/src/pages/KPIDashboardReceiving/KPIDashboardReceiving.jsx
+++ Frontend/src/pages/KPIDashboardReceiving/KPIDashboardReceiving.jsx
@@ -6,9 +6,6 @@ import { ExpectedReceiptsColumn, InternalTransferColumn, TblDispatchingCLColumn,
 import expected from "../../images/expected.png"
 import shipment from "../../images/shipment.png"
 import transfer from "../../images/transfer.png"
-import pdf from "../../images/pdf.png"
-import excel from "../../images/excel.png"
-
 import items from "../../images/items.png"
 
 
@@ -30,42 +27,103 @@ ReactFC.fcRoot(FusionCharts, Components, FusionTheme);
 
 
 const KPIDashboardRece = () => {
-  const [data, setData] = useState(
-    {
-      uniqueShipmentCount: "",
-      totalReceipts: "",
-      totalItems: ""
-    }
-  );
-
+  const [newExpectedReceipts, setNewExpectedReceipts] = useState([]);
+  const [newExpectedShipments, setNewExpectedShipments] = useState([]);
+  const [newItemsDispatch, setNewItemsDispatch] = useState([]);
+  const [newTransferOrder, setNewTransferOrder] = useState([]);
 
   useEffect(() => {
 
     const getAllAssetsList = async () => {
       try {
 
-        userRequest.get("/getTblShipmentReceivedCLStats")
-          .then(response => {
+      userRequest.get("/getAllExpectedShipments")
+        .then(response => {
+          setNewExpectedReceipts(response.data)
+        })
+        .catch(error => {
 
-            setData(response.data)
+
+          console.error(error);
+        });
+
+    }
+    catch (error) {
+      console.log(error);
+
+    }
+  };
+
+  const getAllExpectedShipments = async () => {
+    try {
+
+      userRequest.get("/getAllShipmentDataFromtShipmentReceivingCL")
+        .then(response => {
             console.log(response.data)
+            setNewExpectedShipments(response.data)
+        })
+        .catch(error => {
+          
 
-          })
-          .catch(error => {
-            console.error(error);
-          });
+          console.error(error);
+        });
+        
+    }
+    catch (error) {
+      console.log(error);
 
-      }
-      catch (error) {
-        console.log(error);
+    }
+  };
 
-      }
+  const getNewItemsDispatch = async () => {
+    try {
 
-    };
+      userRequest.get("/getAllTblDispatchingData")
+        .then(response => {
+          // response.data == "no data available" ? setNewItemsDispatch([]) : setNewItemsDispatch(response.data);
+          setNewItemsDispatch(response.data)
+        })
+        .catch(error => {
+          
+          // setError(error?.response?.data?.message ?? "Something went wrong");
 
+          console.error(error);
+        });
+        
+    }
+    catch (error) {
+      console.log(error);
 
-    getAllAssetsList();
-  }, [])
+    }
+  };
+
+  const getNewTransferOrder = async () => {
+    try {
+
+      userRequest.get("/getAllExpectedTransferOrder")
+        .then(response => {
+          // response.data == "no data available" ? setNewTransferOrder([]) : setNewTransferOrder(response.data);
+          setNewTransferOrder(response.data)
+        })
+        .catch(error => {
+          
+          // setError(error?.response?.data?.message ?? "Something went wrong");
+
+          console.error(error);
+        });
+        
+    }
+    catch (error) {
+      console.log(error);
+
+    }
+  };
+
+  getAllAssetsList();
+  getAllExpectedShipments();
+  getNewItemsDispatch();
+  getNewTransferOrder();
+},[])
   return (
     <div>
       <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-white text-black dark:text-white">
@@ -79,17 +137,17 @@ const KPIDashboardRece = () => {
                 <img src={expected} className='' alt='' />
               </div>
               <div className="text-right">
-                <span>{data.totalReceipts}</span>
+                <span>{newExpectedReceipts.length > 0 ? newExpectedReceipts.length : null}</span>
                 <p>Daily Receipts</p>
               </div>
             </div>
             <div className="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
               <div className="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
-                {/* <svg width="30" height="30" fill="no/ne" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg> */}
+                {/* <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="stroke-current text-blue-800 dark:text-gray-800 transform transition-transform duration-500 ease-in-out"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg> */}
                 <img src={shipment} className='' alt='' />
               </div>
               <div className="text-right">
-                <span>{data.uniqueShipmentCount}</span>
+                <span>{newExpectedShipments.length > 0 ? newExpectedShipments.length : null}</span>
                 <p>Total Shipments</p>
               </div>
             </div>
@@ -98,40 +156,43 @@ const KPIDashboardRece = () => {
                 <img src={items} className='' alt='' />
               </div>
               <div className="text-right">
-                <span>{data.totalItems}</span>
+                <span>{newItemsDispatch.length > 0 ? newItemsDispatch.length : null}</span>
                 <p>Total Items</p>
               </div>
             </div>
-            <div className="card-icons-div">
-            
-              <img src={excel} alt="icon" className="icon-image" />
-              <img src={pdf} alt="icon" className="icon-image" />
+            <div className="bg-blue-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white font-medium group">
+              <div className="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
+                <img src={transfer} className='' alt='' />
+              </div>
+              <div className="text-right">
+                <span>{newTransferOrder.length > 0 ? newTransferOrder.length : null}</span>
+                <p>Transfer Orders</p>
+              </div>
             </div>
-
           </div>
           {/* <!-- ./Statistics Cards --> */}
 
           <div className="grid grid-cols-1 md:grid-cols-2 p-4 gap-4 bg-gradient-to-r from-[#5666e2]">
             {/* <!-- Social Traffic --> */}
             <div className="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
-              <DailyReceiptsChart ReactFC={ReactFC} />
+                <DailyReceiptsChart ReactFC={ReactFC}/>
+            </div>
+
+             {/* <!-- Social Traffic2 --> */}
+             <div className="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
+                <TotalShipment ReactFC={ReactFC}/>
             </div>
 
             {/* <!-- Social Traffic2 --> */}
             <div className="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
-              <TotalShipment ReactFC={ReactFC} />
+                <ItemsChart ReactFC={ReactFC}/>
+               {/* <DashboardTable data={newItemsDispatch} columnsName={TblDispatchingCLColumn} title={"Items Dispatch"} UniqueId="assetPrintingId"/> */}
             </div>
 
             {/* <!-- Social Traffic2 --> */}
             <div className="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
-              <ItemsChart ReactFC={ReactFC} />
-              {/* <DashboardTable data={newItemsDispatch} columnsName={TblDispatchingCLColumn} title={"Items Dispatch"} UniqueId="assetPrintingId"/> */}
-            </div>
-
-            {/* <!-- Social Traffic2 --> */}
-            <div className="relative flex flex-col min-w-0 mb-4 lg:mb-0 break-words bg-gray-50 dark:bg-gray-800 w-full shadow-lg rounded">
-              <ShipementReceivedChart ReactFC={ReactFC} />
-              {/* <DashboardTable data={newTransferOrder} columnsName={InternalTransferColumn} title={"Internal Transfer"} UniqueId="assetPrintingId"/> */}
+               <ShipementReceivedChart ReactFC={ReactFC}/>
+               {/* <DashboardTable data={newTransferOrder} columnsName={InternalTransferColumn} title={"Internal Transfer"} UniqueId="assetPrintingId"/> */}
             </div>
 
           </div>

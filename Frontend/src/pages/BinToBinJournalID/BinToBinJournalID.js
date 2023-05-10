2f417fcc17f376dfd97eda7f7d2067abc86437c4
HasnainAhmad12 <hasnainbangash03@gmail.com>
2023-05-10 17:27:42 +0500

    Commit

--- Frontend/src/pages/BinToBinJournalID/BinToBinJournalID.js
+++ Frontend/src/pages/BinToBinJournalID/BinToBinJournalID.js
@@ -7,14 +7,62 @@ import "./BinToBinJournalID.css";
 // import CustomSnakebar from '../../utils/CustomSnakebar';
 // import back from "../../images/back.png"
 import undo from "../../images/undo.png"
+import { SyncLoader } from 'react-spinners';
 
 const BinToBinJournalID = () => {
   const navigate = useNavigate();
- 
+  
+  const [transferTag, setTransferTag] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+  
+
+
+  const handleChangeValue = (e) => {
+    setTransferTag(e.target.value);
+  }
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    setIsLoading(true)
+
+    userRequest.get(`/getTransferJournalCLByJournalId?JournalID=${transferTag}`)
+      .then(response => {
+        console.log(response?.data);
+
+        setData(response?.data ?? []);
+        setIsLoading(false)
+      })
+
+      .catch(error => {
+        console.error(error);
+        setIsLoading(false)
+      });
+  }
+
+
   return (
     <>
-      {/* {message && <CustomSnakebar message={message} severity="success" onClose={resetSnakeBarMessages} />} */}
-      {/* {error && <CustomSnakebar message={error} severity="error" onClose={resetSnakeBarMessages} />} */}
+      
+      {isLoading &&
+
+      <div className='loading-spinner-background'
+          style={{
+              zIndex: 9999, position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(255, 255, 255, 0.5)',
+              display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'fixed'
+
+
+          }}
+      >
+            <SyncLoader
+
+                size={18}
+                color={"#FFA500"}
+                // height={4}
+                loading={isLoading}
+            />
+        </div>
+        }
 
       <div className="bg-black before:animate-pulse before:bg-gradient-to-b before:from-gray-900 overflow-hidden before:via-[#00FF00] before:to-gray-900 before:absolute ">
         <div className="w-full h-auto px-3 sm:px-5 flex items-center justify-center absolute">
@@ -42,6 +90,7 @@ const BinToBinJournalID = () => {
                 <h2 className='text-[#00006A] text-center font-semibold'>Internal AXAPTA<span className='text-[#FF0404]'>*</span></h2>
             </div>
       
+            <form onSubmit={handleForm}>
             <div className='mb-6'>
               <label htmlFor='transfer' className="block mb-2 sm:text-lg text-xs font-medium text-[#00006A]">Journal ID<span className='text-[#FF0404]'>*</span></label>
               <div className='w-full flex'>
@@ -49,26 +98,17 @@ const BinToBinJournalID = () => {
                   id="transfer" 
                     className="bg-gray-50 font-semibold border border-[#00006A] text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 md:p-2.5" 
                       placeholder="Enter /Scan Journal ID "
-                     // value={palletCode}
-                  //    onChange={handleInputChange}
+                        onChange={handleChangeValue}
                   />
                 <button
-                  type='button' 
+                  type='submit' 
                     className='bg-[#F98E1A] hover:bg-[#edc498] text-[#fff] font-medium py-2 px-6 rounded-sm'
                     >
                       FIND
                 </button>        
                   </div> 
             </div>
-
-            {/* <div className='mb-6'>
-                 <div className='flex gap-2 justify-center items-center'>
-                    <span className='text-[#00006A] font-semibold'>FROM:</span>
-                    <input className="bg-gray-50 border border-gray-300 placeholder:text-[#00006A] text-xs rounded-lg focus:ring-blue-500
-                    block w-full p-1.5 md:p-2.5 " placeholder="Location Reference "
-                    />
-                  </div>
-            </div> */}
+            </form>
 
             <div className='mb-6'>
               <label className='text-[#00006A] font-semibold'>List of Items on Bin<span className='text-[#FF0404]'>*</span></label>
@@ -77,9 +117,7 @@ const BinToBinJournalID = () => {
                 <table>
                   <thead>
                     <tr>
-                      <th>ID</th>
                       <th>TransferID</th>
-                      <th>Config</th>
                       <th>InventoryLocationIDFrom</th>
                       <th>InventoryLocationIDTo</th>
                       <th>Item ID </th>
@@ -87,50 +125,31 @@ const BinToBinJournalID = () => {
                       <th>Qty.Transfer</th>
                       <th>Qty.RemainRecieve </th>
                       <th>Created DateTime</th>
+                      <th>JournalID</th>
+                      <th>BinLocation</th>
+                      <th>DateTimeTransaction</th>
+                      <th>CONFIG</th>
+                      <th>USERID</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {/* {serialNumbers.map((serialNumber, index) => (
+                    {data.map((item, index) => (
                       <tr key={index}>
-                        <td>{serialNumber}</td>
+                        <td>{item.TRANSFERID}</td>
+                        <td>{item.TRANSFERSTATUS}</td>
+                        <td>{item.INVENTLOCATIONIDFROM}</td>
+                        <td>{item.INVENTLOCATIONIDTO}</td>
+                        <td>{item.ITEMID}</td>
+                        <td>{item.QTYTRANSFER}</td>
+                        <td>{item.QTYRECEIVED}</td>
+                        <td>{item.CREATEDDATETIME}</td>
+                        <td>{item.JournalID}</td>
+                        <td>{item.BinLocation}</td>
+                        <td>{item.DateTimeTransaction}</td>
+                        <td>{item.CONFIG}</td>
+                        <td>{item.USERID}</td>
                       </tr>
-                    ))} */}
-                    <tr>
-                        <td>1</td>
-                        <td>IRT-00398365</td>
-                        <td>G</td>
-                        <td>RMW01</td>
-                        <td>RSR01</td>
-                        <td>SF-P110XAV 2206 WH</td>
-                        <td>ID00000000003</td>
-                        <td>1.000000000000</td>
-                        <td>1.000000000000</td>
-                        <td>2023-04-28 12:04:13.000</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>IRT-00398365</td>
-                        <td>G</td>
-                        <td>RMW01</td>
-                        <td>RSR01</td>
-                        <td>SF-P110XAV 2206 WH</td>
-                        <td>ID00000000003</td>
-                        <td>1.000000000000</td>
-                        <td>1.000000000000</td>
-                        <td>2023-04-28 12:04:13.000</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>IRT-00398365</td>
-                        <td>G</td>
-                        <td>RMW01</td>
-                        <td>RSR01</td>
-                        <td>SF-P110XAV 2206 WH</td>
-                        <td>ID00000000003</td>
-                        <td>1.000000000000</td>
-                        <td>1.000000000000</td>
-                        <td>2023-04-28 12:04:13.000</td>
-                    </tr>
+                    ))}
                   </tbody>
                 </table>
               </div>

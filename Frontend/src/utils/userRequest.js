2f5bae93270da49ae8a34e7a84937890a6f7f542
unknown <abdulmajid1m2@gmail.com>
2023-05-22 16:25:17 +0500

    teken fix

--- Frontend/src/utils/userRequest.js
+++ Frontend/src/utils/userRequest.js
@@ -2,12 +2,19 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import baseUrl from "./config";
 
-const token = Cookies.get("accessToken");
+// const token = Cookies.get("accessToken");
+
+function getToken() {
+    const token = Cookies.get("accessToken");
+    return token || ""; // Return an empty string if token is undefined
+}
 const userRequest = axios.create({
     baseURL: baseUrl,
     headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${getToken()}`,
     },
 });
+
+
 
 export default userRequest;

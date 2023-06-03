f6c0072b608ae2c71801c20c792b80accfbb6e1f
HasnainAhmad12 <hasnainbangash03@gmail.com>
2023-06-03 18:35:51 +0500

    Commit

--- Frontend/src/pages/UserRegistration/UserRegistration.js
+++ Frontend/src/pages/UserRegistration/UserRegistration.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import alessa from "../../images/alessalogo2.png"
 import "../UserRegistration/UserRegistration.css"
 import Swal from 'sweetalert2'
@@ -16,7 +16,10 @@ const Registration = () => {
   const [UserName, setUserName] = useState('')
   const [UserLevel, setUserLevel] = useState('')
   const [UserLocation, setUserLocation] = useState('')
+  const [roles, setRoles] = useState([]);
+  const [email, setEmail] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  
 
   const navigate = useNavigate();
 
@@ -31,12 +34,13 @@ const Registration = () => {
           Fullname: UserName,
           UserLevel: UserLevel,
           Loc: UserLocation,
+          Email: email,
         },
       })
       .then((response) => {
         Cookies.set("accessToken", response?.data?.token);
         localStorage.setItem("currentUser", JSON.stringify(response?.data?.user));
-        navigate("/dashboard");
+        navigate("/");
         setIsLoading(false)
 
       })
@@ -50,6 +54,20 @@ const Registration = () => {
         })
       });
   }
+
+
+
+  useEffect(() => {
+      userRequest
+        .get("/getAlltblRoles")
+        .then((response) => {
+          setRoles(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  }, []);
+
 
   return (
     <div className='login_img_section'>
@@ -104,9 +122,9 @@ const Registration = () => {
                 <img src={alessa} className='h-24 w-auto' alt='' />
               </div>
               {/* <h1 className="text-gray-800 font-bold text-2xl mb-1">Hello Again!</h1> */}
-              <p className="text-sm font-normal text-gray-600 mb-8">Welcome Back WBS</p>
+              <p className="text-sm font-normal text-gray-600 mb-6">Welcome Back WBS</p>
 
-              <div className="flex items-center border-2 mb-5 py-2 px-3 rounded-2xl">
+              <div className="flex items-center border-2 mb-4 py-2 px-3 rounded-2xl">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
                 </svg>
@@ -117,7 +135,7 @@ const Registration = () => {
                 />
               </div>
 
-              <div className="flex items-center border-2 mb-9 py-2 px-3 rounded-2xl ">
+              <div className="flex items-center border-2 mb-8 py-2 px-3 rounded-2xl ">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
                 </svg>
@@ -128,7 +146,7 @@ const Registration = () => {
 
               </div>
 
-              <div className="flex items-center border-2 mb-9 -mt-3 py-2 px-3 rounded-2xl">
+              <div className="flex items-center border-2 mb-8 -mt-3 py-2 px-3 rounded-2xl">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
                 </svg>
@@ -139,18 +157,40 @@ const Registration = () => {
                 />
               </div>
 
-              <div className="flex items-center border-2 mb-9 -mt-3 py-2 px-3 rounded-2xl">
+              <div className="flex items-center border-2 mb-8 -mt-3 py-2 px-3 rounded-2xl">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
                 </svg>
-                <input id="userlevel"
-                  onChange={(e) => setUserLevel(e.target.value)}
-                  className=" pl-2 w-full outline-none border-none" type="text" placeholder="User Level"
+                <select
+                  id="userlevel"
+                   onChange={(e) => setUserLevel(e.target.value)}
+                    className="pl-2 w-full outline-none border-none"
+                      type="text"
+                        placeholder="User Level"
+                         required
+                >
+
+                  <option value="0">Select User Level</option>
+                  {roles.map((role) => (
+                    <option key={role.id} value={role.id}>
+                      {role.RoleName}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="flex items-center border-2 mb-8 -mt-3 py-2 px-3 rounded-2xl">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+                </svg>
+                <input id="useremail"
+                  onChange={(e) => setEmail(e.target.value)}
+                  className=" pl-2 w-full outline-none border-none" type="text" placeholder="User Email"
                   required
                 />
               </div>
 
-              <div className="flex items-center border-2 mb-9 -mt-3 py-2 px-3 rounded-2xl">
+              <div className="flex items-center border-2 mb-6 -mt-3 py-2 px-3 rounded-2xl">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
                 </svg>
@@ -163,7 +203,7 @@ const Registration = () => {
 
               <button
                 type="submit"
-                className="block w-full bg-[#FFA500] mt-5 py-2 rounded-2xl hover:bg-[#7b663e] hover:-translate-y-1 transition-all duration-500 text-white font-semibold mb-2"
+                className="block w-full bg-[#FFA500] mt-5 py-2 rounded-2xl hover:bg-[#7b663e] hover:-translate-y-1 transition-all duration-500 text-white font-semibold mb-0"
               >Register</button>
 
             </form>

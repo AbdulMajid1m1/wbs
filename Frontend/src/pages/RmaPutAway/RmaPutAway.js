30b8d390dad426ca548e2c60978a89a63de9d3b5
Hasnain Bangash <hasnainbangash03@gmail.com>
2023-07-25 14:21:14 +0500

    Commit

--- Frontend/src/pages/RmaPutAway/RmaPutAway.js
+++ Frontend/src/pages/RmaPutAway/RmaPutAway.js
@@ -12,7 +12,7 @@ const WmsCycleCounting = () => {
   const navigate = useNavigate();
 
   const autocompleteRef = useRef();
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [message, setMessage] = useState(null);
@@ -62,17 +62,75 @@ const WmsCycleCounting = () => {
     getLocationData();
     getRmaReturn();
   }, []);
+  // const handleFormSubmit = async (e) => {
+  //   e.preventDefault();
+  //   if (!binlocation) {
+  //     setError("Please enter bin location");
+  //     return;
+  //   }
+  //   setIsLoading(true);
+
+  //   const mappedData = data.map((item) => ({
+  //     // itemcode: item?.ITEMID
+  //     // if null then don't send itemcode in the request
+  //     ...(item?.ITEMID && { itemcode: item?.ITEMID }),
+  //     itemdesc: item?.NAME,
+  //     classification: item?.RETURNITEMNUM,
+  //     mainlocation: item?.INVENTSITEID,
+  //     binlocation: binlocation,
+  //     intcode: item?.CONFIGID,
+  //     itemserialno: item?.ITEMSERIALNO,
+  //     mapdate: item?.TRXDATETIME ?? "",
+  //     user: item?.ASSIGNEDTOUSERID ?? "",
+  //     gtin: "", // Add any default or empty values for the new fields in the API
+  //     remarks: "",
+  //     palletcode: "",
+  //     reference: "",
+  //     sid: "",
+  //     cid: "",
+  //     po: "",
+
+  //   }));
+  //   console.log(mappedData);
+
+  //   try {
+  //     const res = await userRequest.post("/insertManyIntoMappedBarcode", { records: mappedData });
+  //     console.log(res?.data);
+  //     setIsLoading(false);
+  //     setMessage("Data Inserted Successfully.");
+  //     // Clear form fields and data state
+  //     setBinlocation("");
+  //     setData([]);
+  //   } catch (error) {
+  //     console.log(error);
+  //     setError(error?.response?.data?.message ?? "Something went wrong");
+  //     setIsLoading(false);
+  //   }
+  // };
+  const handleFromSelect = (event, value) => {
+    setBinlocation(value);
+
+  };
+
+
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     if (!binlocation) {
       setError("Please enter bin location");
       return;
     }
-    setIsLoading(true);
 
-    const mappedData = data.map((item) => ({
-      // itemcode: item?.ITEMID
-      // if null then don't send itemcode in the request
+    // Check if any row is selected
+    if (selectedRows.length === 0) {
+      setError("Please select at least one row");
+      return;
+    }
+    
+    setIsLoading(true);
+  
+    const selectedData = selectedRows.map((index) => data[index]); // Filter the selected rows from the data array
+  
+    const mappedData = selectedData.map((item) => ({
       ...(item?.ITEMID && { itemcode: item?.ITEMID }),
       itemdesc: item?.NAME,
       classification: item?.RETURNITEMNUM,
@@ -82,17 +140,16 @@ const WmsCycleCounting = () => {
       itemserialno: item?.ITEMSERIALNO,
       mapdate: item?.TRXDATETIME ?? "",
       user: item?.ASSIGNEDTOUSERID ?? "",
-      gtin: "", // Add any default or empty values for the new fields in the API
+      gtin: "",
       remarks: "",
       palletcode: "",
       reference: "",
       sid: "",
       cid: "",
       po: "",
-
     }));
     console.log(mappedData);
-
+  
     try {
       const res = await userRequest.post("/insertManyIntoMappedBarcode", { records: mappedData });
       console.log(res?.data);
@@ -101,17 +158,36 @@ const WmsCycleCounting = () => {
       // Clear form fields and data state
       setBinlocation("");
       setData([]);
+      setSelectedRows([]); // Clear selected rows after successful insertion
     } catch (error) {
       console.log(error);
       setError(error?.response?.data?.message ?? "Something went wrong");
       setIsLoading(false);
     }
   };
-  const handleFromSelect = (event, value) => {
-    setBinlocation(value);
+  
 
+
+   // State to keep track of the selected rows
+   const [selectedRows, setSelectedRows] = useState([]);
+
+    // Function to handle row selection
+  // const handleRowSelect = (index) => {
+  //   if (selectedRows.includes(index)) {
+  //     setSelectedRows(selectedRows.filter((rowIndex) => rowIndex !== index));
+  //   } else {
+  //     setSelectedRows([...selectedRows, index]);
+  //   }
+  // };
+
+  const handleRowSelect = (index) => {
+    if (selectedRows.includes(index)) {
+      setSelectedRows(selectedRows.filter((rowIndex) => rowIndex !== index));
+    } else {
+      setSelectedRows([...selectedRows, index]);
+    }
   };
-
+  
 
 
   return (
@@ -141,7 +217,7 @@ const WmsCycleCounting = () => {
       }
 
       <div className="bg-black before:animate-pulse before:bg-gradient-to-b before:from-gray-900 overflow-hidden before:via-[#00FF00] before:to-gray-900 before:absolute ">
-        <div className="w-full h-auto px-3 sm:px-5 flex items-center justify-center absolute">
+        <div className="w-full h-auto sm:px-5 flex items-center justify-center absolute">
           <div className="w-full sm:w-1/2 lg:2/3 px-6 bg-gray-400 bg-opacity-20 bg-clip-padding backdrop-filter backdrop-blur-sm text-white z-50 py-4  rounded-lg">
             <div className="w-full font-semibold p-6 shadow-xl rounded-md text-black bg-[#F98E1A] text-xl mb:2 md:mb-5">
 
@@ -164,54 +240,63 @@ const WmsCycleCounting = () => {
             </div>
 
 
-
-
-            <div className='mb-6'>
-              <label className='text-[#00006A] font-semibold'>List of RMA PutAway<span className='text-[#FF0404]'>*</span></label>
-              {/* // creae excel like Tables  */}
-              <div className="table-location-generate1">
-                <table>
-                  <thead>
-                    <tr>
-                      <th>ITEMID</th>
-                      <th>NAME</th>
-                      <th>EXPECTEDRETQTY</th>
-                      <th>SALESID</th>
-                      <th>RETURNITEMNUM</th>
-                      <th>INVENTSITEID</th>
-                      <th>INVENTLOCATIONID</th>
-                      <th>CONFIGID</th>
-                      <th>WMSLOCATIONID</th>
-                      <th>ASSIGNEDTOUSERID</th>
-                      <th>TRXDATETIME</th>
-                      <th>TRXUSERID</th>
-                      <th>ITEMSERIALNO</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {data.map((item, index) => (
-                      <tr key={index}>
-                        <td>{item.ITEMID}</td>
-                        <td>{item.NAME}</td>
-                        <td>{item.EXPECTEDRETQTY}</td>
-                        <td>{item.SALESID}</td>
-                        <td>{item.RETURNITEMNUM}</td>
-                        <td>{item.INVENTSITEID}</td>
-                        <td>{item.INVENTLOCATIONID}</td>
-                        <td>{item.CONFIGID}</td>
-                        <td>{item.WMSLOCATIONID}</td>
-                        <td>{item.ASSIGNEDTOUSERID}</td>
-                        <td>{item.TRXDATETIME}</td>
-                        <td>{item.TRXUSERID}</td>
-                        <td>{item.ITEMSERIALNO}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-
-
-              </div>
-            </div>
+<div className='mb-6'>
+      <label className='text-[#00006A] font-semibold'>List of RMA PutAway<span className='text-[#FF0404]'>*</span></label>
+      {/* // create excel-like Tables  */}
+      <div className="table-location-generate1">
+        <table>
+          <thead>
+            <tr>
+              <th>Select</th>
+              <th>ITEMID</th>
+              <th>NAME</th>
+              <th>EXPECTEDRETQTY</th>
+              <th>SALESID</th>
+              <th>RETURNITEMNUM</th>
+              <th>INVENTSITEID</th>
+              <th>INVENTLOCATIONID</th>
+              <th>CONFIGID</th>
+              <th>WMSLOCATIONID</th>
+              <th>ASSIGNEDTOUSERID</th>
+              <th>TRXDATETIME</th>
+              <th>TRXUSERID</th>
+              <th>ITEMSERIALNO</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr
+                key={index}
+                // Apply a different class to the selected row
+                className={selectedRows.includes(index) ? 'selected' : ''}
+                onClick={() => handleRowSelect(index)}
+              >
+                <td>
+                  <input
+                    type="checkbox"
+                    checked={selectedRows.includes(index)}
+                    onChange={() => handleRowSelect(index)}
+                  />
+                </td>
+                <td>{item.ITEMID}</td>
+                <td>{item.NAME}</td>
+                <td>{item.EXPECTEDRETQTY}</td>
+                <td>{item.SALESID}</td>
+                <td>{item.RETURNITEMNUM}</td>
+                <td>{item.INVENTSITEID}</td>
+                <td>{item.INVENTLOCATIONID}</td>
+                <td>{item.CONFIGID}</td>
+                <td>{item.WMSLOCATIONID}</td>
+                <td>{item.ASSIGNEDTOUSERID}</td>
+                <td>{item.TRXDATETIME}</td>
+                <td>{item.TRXUSERID}</td>
+                <td>{item.ITEMSERIALNO}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
 
           
 

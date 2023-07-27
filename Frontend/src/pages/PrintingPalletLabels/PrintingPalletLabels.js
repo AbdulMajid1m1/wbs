0d3e0c90dd39ce13e161b6aab05d015268a37593
unknown <abdulmajid1m2@gmail.com>
2023-07-27 09:58:34 +0500

    added mui loaders and integrated filters with apis

--- Frontend/src/pages/PrintingPalletLabels/PrintingPalletLabels.js
+++ Frontend/src/pages/PrintingPalletLabels/PrintingPalletLabels.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import UserDataTable from '../../components/UserDatatable/UserDataTable'
 import { MappedItemsColumn } from '../../utils/datatablesource'
 import userRequest from "../../utils/userRequest"
-import { SyncLoader } from 'react-spinners';
 import CustomSnakebar from '../../utils/CustomSnakebar';
 
 const PrintingPalletLabels = () => {
@@ -60,29 +59,12 @@ const PrintingPalletLabels = () => {
                 printButton={true}
                 PrintName={"Print Pallet Labels"}
                 TotalCount={count}
+                loading={isLoading}
+                setIsLoading={setIsLoading}
             //    printBarCode={true}
             //    PrintBarCodeName={"Print Pallet Barcode"}
             />
 
-            {isLoading &&
-
-                <div className='loading-spinner-background'
-                    style={{
-                        zIndex: 9999, position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(255, 255, 255, 0.5)',
-                        display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'fixed'
-
-
-                    }}
-                >
-                    <SyncLoader
-
-                        size={18}
-                        color={"#FFA500"}
-                        // height={4}
-                        loading={isLoading}
-                    />
-                </div>
-            }
 
         </div>
     )

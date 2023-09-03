087833bf6c6c7fbc9829097e6295bcc19742aee1
unknown <abdulmajid1m2@gmail.com>
2023-09-03 10:16:17 +0500

    d

--- Frontend/src/pages/PrintingItemLabels/PrintingItemLabels.js
+++ Frontend/src/pages/PrintingItemLabels/PrintingItemLabels.js
@@ -8,106 +8,110 @@ import { QRCodeSVG } from "qrcode.react";
 
 
 const PrintingItemLabels = () => {
-   const navigate = useNavigate();
-   const [message, setMessage] = useState(null);
-   const [error, setError] = useState(null);
-   const [userGtinData, setUserGtinData] = useState('');
-   const [serialNumber, setSerialNumber] = useState('');
-   const [itemCode, setItemCode] = useState('');
- 
-   const resetSnakeBarMessages = () => {
-    setError(null);
-    setMessage(null);
+    const navigate = useNavigate();
+    const [message, setMessage] = useState(null);
+    const [error, setError] = useState(null);
+    const [userGtinData, setUserGtinData] = useState('');
+    const [serialNumber, setSerialNumber] = useState('');
+    const [itemCode, setItemCode] = useState('');
 
-  };
-   
+    const resetSnakeBarMessages = () => {
+        setError(null);
+        setMessage(null);
 
-   const handlePrintGtin = () => {
-    if (userGtinData.length === 0) {
-      setError('Please Enter Gtin Number.');
-      return;
-    }
-    const printWindow = window.open('', 'Print Window', 'height=400,width=800');
-    const html = '<html><head><title>Print Barcode</title>' +
-      '<style>' +
-      '@page { size: 3in 2in; margin: 0; }' +
-      'body { font-size: 13px; line-height: 0.1;}' +
-      '#header { display: flex; justify-content: center;}' +
-      '#imglogo {height: 40px; width: 100px;}' +
-      '#itemcode { font-size: 13px; font-weight: 600; display: flex; justify-content: center;}' +
-      '#inside-BRCode { display: flex; justify-content: center; align-items: center; padding: 1px;}' +
-      '#gtinNo { font-size: 13px; display: flex; justify-content: center; font-weight: 600; margin-top: 3px;}' +
-      '#main-print { height: 100%; width: 100%;}' +
-      '</style>' +
-      '</head><body>' +
-      '<div id="printBarcode"></div>' +
-      '</body></html>';
-
-    printWindow.document.write(html);
-    const barcodeContainer = printWindow.document.getElementById('printBarcode');
-    const barcode = document.getElementById('barcode').cloneNode(true);
-    barcodeContainer.appendChild(barcode);
-
-    const logoImg = new Image();
-    logoImg.src = logo;
-
-    logoImg.onload = function () {
-      printWindow.document.getElementById('imglogo').src = logoImg.src;
-
-      printWindow.print();
-      printWindow.close();
-
-      
     };
-  }
 
 
- 
-  const handlePrintItemBarcode = () => {
-    if (itemCode.length === 0) {
-      setError('Please Enter ItemCode.');
-      return;
+    const handlePrintGtin = () => {
+        if (userGtinData.length === 0) {
+            setError('Please Enter Gtin Number.');
+            return;
+        }
+        const printWindow = window.open('', 'Print Window', 'height=400,width=800');
+        const html = '<html><head><title>Print Barcode</title>' +
+            '<style>' +
+            '@page { size: 3in 2in; margin: 0; }' +
+            'body { font-size: 13px; line-height: 0.1;}' +
+            '#header { display: flex; justify-content: center;}' +
+            '#imglogo {height: 40px; width: 100px;}' +
+            '#itemcode { font-size: 13px; font-weight: 600; display: flex; justify-content: center;}' +
+            '#inside-BRCode { display: flex; justify-content: center; align-items: center; padding: 1px;}' +
+            '#gtinNo { font-size: 13px; display: flex; justify-content: center; font-weight: 600; margin-top: 3px;}' +
+            '#main-print { height: 100%; width: 100%;}' +
+            '</style>' +
+            '</head><body>' +
+            '<div id="printBarcode"></div>' +
+            '</body></html>';
+
+        printWindow.document.write(html);
+        const barcodeContainer = printWindow.document.getElementById('printBarcode');
+        const barcode = document.getElementById('barcode').cloneNode(true);
+        barcodeContainer.appendChild(barcode);
+
+        const logoImg = new Image();
+        logoImg.src = logo;
+
+        logoImg.onload = function () {
+            printWindow.document.getElementById('imglogo').src = logoImg.src;
+
+            printWindow.print();
+            printWindow.close();
+
+            setTimeout(() => {
+                setUserGtinData('');
+            }, 500);
+
+
+        };
     }
-    if (serialNumber.length === 0) {
-        setError('Please Serial Number.');
-        return;
-      }
-    const printWindow = window.open('', 'Print Window', 'height=400,width=800');
-    const html = '<html><head><title>Print Barcode</title>' +
-      '<style>' +
-      '@page { size: 3in 2in; margin: 0; }' +
-      'body { font-size: 13px; line-height: 0.1;}' +
-      '#header { display: flex; justify-content: center;}' +
-      '#imglogo {height: 40px; width: 100px;}' +
-      '#itemcode { font-size: 13px; font-weight: 600; display: flex; justify-content: center;}' +
-      '#inside-BRCode { display: flex; justify-content: center; align-items: center; padding: 1px;}' +
-      '#itemSerialNo { font-size: 13px; display: flex; justify-content: center; font-weight: 600; margin-top: 3px;}' +
-      '#main-print { height: 100%; width: 100%;}' +
-      '</style>' +
-      '</head><body>' +
-      '<div id="printBarcode"></div>' +
-      '</body></html>';
 
-    printWindow.document.write(html);
-    const barcodeContainer = printWindow.document.getElementById('printBarcode');
-    const barcode = document.getElementById('QrcodeGtin').cloneNode(true);
-    barcodeContainer.appendChild(barcode);
 
-    const logoImg = new Image();
-    logoImg.src = logo;
 
-    logoImg.onload = function () {
-      printWindow.document.getElementById('imglogo').src = logoImg.src;
-      printWindow.print();
-      printWindow.close();
-    //   setTimeout(() => {
-    //     setSelectedRow([]);
-    //     setRowSelectionModel([]);
+    const handlePrintItemBarcode = () => {
+        if (itemCode.length === 0) {
+            setError('Please Enter ItemCode.');
+            return;
+        }
+        if (serialNumber.length === 0) {
+            setError('Please Serial Number.');
+            return;
+        }
+        const printWindow = window.open('', 'Print Window', 'height=400,width=800');
+        const html = '<html><head><title>Print Barcode</title>' +
+            '<style>' +
+            '@page { size: 3in 2in; margin: 0; }' +
+            'body { font-size: 13px; line-height: 0.1;}' +
+            '#header { display: flex; justify-content: center;}' +
+            '#imglogo {height: 40px; width: 100px;}' +
+            '#itemcode { font-size: 13px; font-weight: 600; display: flex; justify-content: center;}' +
+            '#inside-BRCode { display: flex; justify-content: center; align-items: center; padding: 1px;}' +
+            '#itemSerialNo { font-size: 13px; display: flex; justify-content: center; font-weight: 600; margin-top: 3px;}' +
+            '#main-print { height: 100%; width: 100%;}' +
+            '</style>' +
+            '</head><body>' +
+            '<div id="printBarcode"></div>' +
+            '</body></html>';
 
-    //   }, 500);
-    };
-  }
- 
+        printWindow.document.write(html);
+        const barcodeContainer = printWindow.document.getElementById('printBarcode');
+        const barcode = document.getElementById('QrcodeGtin').cloneNode(true);
+        barcodeContainer.appendChild(barcode);
+
+        const logoImg = new Image();
+        logoImg.src = logo;
+
+        logoImg.onload = function () {
+            printWindow.document.getElementById('imglogo').src = logoImg.src;
+            printWindow.print();
+            printWindow.close();
+
+            setTimeout(() => {
+                setItemCode('');
+                setSerialNumber('');
+            }, 500);
+        };
+    }
+
     return (
         <>
             {message && <CustomSnakebar message={message} severity="success" onClose={resetSnakeBarMessages} />}
@@ -130,15 +134,15 @@ const PrintingItemLabels = () => {
 
 
                             <div className="right">
-                                
+
                                 <form id="myForm" >
-                                        <div className="formInput">
-                                            <label>Enter GTIN To Print<span className="text-red-500 font-semibold">*</span></label>
-                                            <input type="text" onChange={(e) => setUserGtinData(e.target.value)} placeholder="Enter GTIN Number"/>
-                                        </div>
+                                    <div className="formInput">
+                                        <label>Enter GTIN To Print<span className="text-red-500 font-semibold">*</span></label>
+                                        <input type="text" onChange={(e) => setUserGtinData(e.target.value)} placeholder="Enter GTIN Number" value={userGtinData} />
+                                    </div>
                                     <div className="buttonAdd">
                                         <button
-                                            style={{background: '#e69138'}}
+                                            style={{ background: '#e69138' }}
                                             type="button"
                                             onClick={handlePrintGtin}
                                         >Print</button>
@@ -166,18 +170,18 @@ const PrintingItemLabels = () => {
                         <div className="bottom">
 
 
-                            <div className="right">      
+                            <div className="right">
                                 <form id="myForm" >
                                     <div className="formInput">
                                         <label className="mt-5">Enter item ID/Code<span className="text-red-500 font-semibold">*</span></label>
-                                        <input type="text" onChange={(e) => setItemCode(e.target.value)} placeholder="Enter item ID/Code"/>
+                                        <input type="text" onChange={(e) => setItemCode(e.target.value)} placeholder="Enter item ID/Code" value={itemCode} />
 
                                         <label className="mt-5">Enter Serial Number<span className="text-red-500 font-semibold">*</span></label>
-                                        <input type="text" onChange={(e) => setSerialNumber(e.target.value)} placeholder="Enter Serial Number"/>
+                                        <input type="text" onChange={(e) => setSerialNumber(e.target.value)} placeholder="Enter Serial Number" value={serialNumber} />
                                     </div>
                                     <div className="buttonAdd">
                                         <button
-                                            style={{background: '#e69138'}}
+                                            style={{ background: '#e69138' }}
                                             type="button"
                                             onClick={handlePrintItemBarcode}
                                         >Print</button>
@@ -196,40 +200,40 @@ const PrintingItemLabels = () => {
             <div id="main-print">
                 <div id="barcode" className='hidden'>
                     <div id='header'>
-                    <div>
-                        <img src={logo} id='imglogo' alt='' />
-                    </div>
+                        <div>
+                            <img src={logo} id='imglogo' alt='' />
+                        </div>
                     </div>
                     {/* <div>
                     <p id="itemcode">{selectedRow.data.ItemCode}</p>
                     </div> */}
                     <div id='inside-BRCode'>
-                        <Barcode value={userGtinData} width={1} height={50} fontSize={14}/>
+                        <Barcode value={userGtinData} width={1} height={50} fontSize={14} />
                     </div>
                 </div>
             </div>
 
 
 
-               <div id="main-print">
-                    <div id="QrcodeGtin" className='hidden'>
-                        <div id='header'>
-                            <div>
-                                <img src={logo} id='imglogo' alt='' />
-                            </div>
-                        </div>
+            <div id="main-print">
+                <div id="QrcodeGtin" className='hidden'>
+                    <div id='header'>
                         <div>
-                            <p id="itemcode">{itemCode}</p>
-                        </div>
-                        <div id='inside-BRCode'>
-                           <QRCodeSVG value={serialNumber} width={100} height={50} />
-                        </div>
-                        
-                        <div id="itemSerialNo">
-                            <p>{serialNumber}</p>
+                            <img src={logo} id='imglogo' alt='' />
                         </div>
                     </div>
+                    <div>
+                        <p id="itemcode">{itemCode}</p>
                     </div>
+                    <div id='inside-BRCode'>
+                        <QRCodeSVG value={serialNumber} width={100} height={50} />
+                    </div>
+
+                    <div id="itemSerialNo">
+                        <p>{serialNumber}</p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };

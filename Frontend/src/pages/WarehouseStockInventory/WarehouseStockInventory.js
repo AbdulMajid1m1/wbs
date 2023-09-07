0eea41546946ac5547ade201262a94cce66e324f
fawadshah95 <fawad.shah@enviovista.com>
2023-09-07 16:08:59 +0500

    Commit

--- Frontend/src/pages/WarehouseStockInventory/WarehouseStockInventory.js
+++ Frontend/src/pages/WarehouseStockInventory/WarehouseStockInventory.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import UserDataTable from '../../components/UserDatatable/UserDataTable'
-import { WarehouseStockInventoryColumn, WarehouseStockInventoryLocationColumn, WarehouseWmsInventoryColumn } from '../../utils/datatablesource'
+import { WarehouseStockInventoryColumn, WarehouseStockInventoryLocationColumn, } from '../../utils/datatablesource'
 import userRequest from "../../utils/userRequest"
-import { SyncLoader } from 'react-spinners';
 import CustomSnakebar from '../../utils/CustomSnakebar';
 
 
@@ -111,25 +110,7 @@ const WarehouseStockInventory = () => {
              
             />
 
-            {/* {isLoading &&
 
-                <div className='loading-spinner-background'
-                    style={{
-                        zIndex: 9999, position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(255, 255, 255, 0.5)',
-                        display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'fixed'
-
-
-                    }}
-                >
-                    <SyncLoader
-
-                        size={18}
-                        color={"#FFA500"}
-                        // height={4}
-                        loading={isLoading}
-                    />
-                </div>
-            } */}
 
         </div>
     )
